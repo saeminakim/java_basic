770153bacb200de93c5fe2dc4ec9adcc0264c8c2
Lily <helloimlily@gmail.com>
2021-08-30 12:29:17 +0900

    0830 Instance & Constructor

--- Chapter5/src/classpart/Student.java
+++ Chapter5/src/classpart/Student.java
@@ -7,6 +7,14 @@ public class Student {
 	int grade;
 	String address;
 	
+	public Student() {}
+	
+	public Student(int id, String name) {
+		studentID = id;
+		studentName = name;
+	}
+	// 생성자 오버로딩 : 매개변수가 다른 생성자가 있는 경우
+	
 	public void showStudentInfo() {
 		System.out.println(studentName + ", " + address);
 	}
@@ -18,4 +26,5 @@ public class Student {
 	public void setStudentName(String name) {
 		this.studentName = name;
 	}
+	
 }
--- Chapter5/src/classpart/StudentTest.java
+++ Chapter5/src/classpart/StudentTest.java
@@ -3,11 +3,22 @@ package classpart;
 public class StudentTest {
 
 	public static void main(String[] args) {
-		Student studentLee = new Student();
-		studentLee.studentName = "이순신";
-		studentLee.address = "서울시 노원구";
+		
+		Student studentLee = new Student(100, "이순신"); // heap 메모리에 studentLee(참조변수) 생성
+//		studentLee.studentName = "이순신";
+//		studentLee.studentID = 100;
+		studentLee.address = "28 The Broadway London";
+		
+		Student studentKim = new Student(); 
+		studentKim.studentName = "김유신";
+		studentKim.studentID = 101;
+		studentKim.address = "28 The Broadway London";
 		
 		studentLee.showStudentInfo();
+		studentKim.showStudentInfo();
+		
+		System.out.println(studentLee); // 해당 변수가 가리키고 있는 heap메모리 주소(참조값)가 출력됨
+		System.out.println(studentKim);
 	}
 
 }
