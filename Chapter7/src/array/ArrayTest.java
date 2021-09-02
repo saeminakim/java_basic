64e499aca0c512d609eaa915960d401a686e9bc5
Lily <helloimlily@gmail.com>
2021-09-02 12:40:51 +0900

    0902 Array

---
+++ Chapter7/src/array/ArrayTest.java
@@ -0,0 +1,23 @@
+package array;
+
+public class ArrayTest {
+
+	public static void main(String[] args) {
+
+		// int[] numbers = new int[10];
+		// int[] numbers = new int[] {0, 1, 2};
+		// int[] numbers = {0, 1, 2};
+		
+		int[] numbers = new int[3];
+		
+		numbers[0] = 1;
+		numbers[1] = 2;
+		numbers[2] = 3;
+		
+		for(int i = 0; i < numbers.length; i++) {
+			System.out.println(numbers[i]);
+		}
+		
+	}
+
+}
