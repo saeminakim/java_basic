0505ac66ff4bba1173f4d94ee2779059b9fab7ed
Lily <helloimlily@gmail.com>
2021-09-07 15:07:28 +0900

    0907 Polymorphism and down casting

--- Chapter8/src/inheritance/AnimalTest.java
+++ Chapter8/src/inheritance/AnimalTest.java
@@ -12,17 +12,29 @@ class Human extends Animal {
 	public void move() {
 		System.out.println("사람이 두 발로 걷습니다.");
 	}
+	
+	public void readBook() {
+		System.out.println("사람이 책을 읽습니다.");
+	}
 }
 
 class Tiger extends Animal {
 	public void move() {
 		System.out.println("호랑이가 네 발로 뜁니다.");
 	}
+	
+	public void hunt() {
+		System.out.println("호랑이가 사냥을 합니다.");
+	}
 }
 
 class Eagle extends Animal {
 	public void move() {
 		System.out.println("독수리가 하늘을 납니다.");
+	}
+	
+	public void fly() {
+		System.out.println("독수리는 날 수 있습니다.");
 	}
 }
 
@@ -48,6 +60,22 @@ public class AnimalTest {
 	public void moveAnimal(Animal animal) {
 		
 		animal.move();
+		
+//		Human human = (Human) animal;
+//		이렇게 할 경우 잘 실행되지만 human이 아닌 애들 tiger, eagle이 생성될 때 에러 발생
+		
+		if(animal instanceof Human) {
+			Human human = (Human) animal;
+			human.readBook();
+		} else if (animal instanceof Tiger) {
+			Tiger tiger = (Tiger) animal;
+			tiger.hunt();
+		} else if (animal instanceof Eagle) {
+			Eagle eagle = (Eagle) animal;
+			eagle.fly();
+		} else {
+			System.out.println("지원되지 않는 기능입니다.");
+		}
 	}
 
 }
